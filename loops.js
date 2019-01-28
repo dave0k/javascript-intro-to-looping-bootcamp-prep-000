6e83eb0452627286decdb55ac2d28642414b32c9
Dave K <dkorff12@gmail.com>
2019-01-28 21:55:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,14 +17,8 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  var i = 0
-  
-  function incrementVariable() {
-    i = i + 1;
-  }
-  
   do {
     array.pop()
-  } while (array.length > 0 && incrementVariable())
+  } while (array.length > 0)
   return array
 }
